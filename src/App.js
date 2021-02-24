63fef7ccd7e90bfb753205b77a2be0a876781d20
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2021-02-24 13:16:12 -0500

    add @

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     return (
       <div className="App">
           <div style={{width: "fit-content"}}>
+            <p className="at">Created by: <a href="https://github.com/spencerlepine">@SpencerLepine</a></p>
             <Board />
           </div>
       </div>
